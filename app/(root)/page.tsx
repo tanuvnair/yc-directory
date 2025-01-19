3d52bb0dc0c9c538d18a0efb3dfe3521286204fa
Tanuv Nair <tanuvnair@gmail.com>
2025-01-20 04:16:14 +0530

    Added search form, all startups and startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,26 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ query?: string }>;
+}) {
+    const query = (await searchParams).query;
+
+    const posts = [
+        {
+            _createdAt: new Date(),
+            views: 55,
+            author: { _id: 1, name: "Tanuv" },
+            _id: 1,
+            description: "This is a description",
+            image: "https://placehold.co/128x128",
+            category: "Tech",
+            title: "We Robots",
+        },
+    ];
+
     return (
         <>
             <section className="pink_container">
@@ -10,6 +32,24 @@ export default function Home() {
                     Submit Ideas, Vote on Pitches, and Get Notices in Virtual
                     Competitions.
                 </p>
+
+                <SearchForm query={query} />
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
             </section>
         </>
     );
---
+++ components/SearchForm.tsx
@@ -0,0 +1,25 @@
+import SearchFormReset from "@/components/SearchFormReset";
+import { Search } from "lucide-react";
+import Form from "next/form";
+
+const SearchForm = ({ query }: { query?: string }) => {
+    return (
+        <Form action="/" scroll={false} className="search-form">
+            <input
+                name="query"
+                defaultValue={query}
+                className="search-input"
+                placeholder="Search Startups"
+            ></input>
+            <div className="flex gap-2">
+                {query && <SearchFormReset />}
+
+                <button type="submit" className="search-btn text-white">
+                    <Search className="size-5" />
+                </button>
+            </div>
+        </Form>
+    );
+};
+
+export default SearchForm;
